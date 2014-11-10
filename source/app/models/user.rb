b347faff7200b6350ef2a6e89ba6cf7df097a81e
Don Pflaster <dpflaster@gmail.com>
2014-11-10 18:30:58 -0500

    Finish seed and associations

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  has_many :charges, :as => :customer
 end
--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 1 company with 5 successful
+company1 = Company.create(name: 'Awesome Company')
+company1.charges.create([
+  [amount:50,paid:true,refunded:false],
+  [amount:60,paid:true,refunded:false],
+  [amount:70,paid:true,refunded:false],
+  [amount:80,paid:true,refunded:false],
+  [amount:90,paid:true,refunded:false]
+]
+)
+
+# 1 company with 3 successful, one disputed, one failed
+company2 = Company.create(name: 'So-so company')
+company2.charges.create([
+  [amount:50,paid:true,refunded:false],
+  [amount:60,paid:true,refunded:false],
+  [amount:70,paid:true,refunded:false],
+  [amount:80,paid:true,refunded:true],
+  [amount:90,paid:false]
+]
+)
+
+# 5 users with 1 successful charge
+5.times { |t|
+  User.create(
+    first_name:'User',last_name:['Person',t+1].join,charges:[
+      Charge.create(amount:20,paid:true,refunded:false)
+    ]
+  )
+}
+
+# 1 user with 2 disputed, 1 failed
+questionable_user = User.create(first_name: 'Questionable', last_name:'User')
+questionable_user.charges.create([
+  [amount:70,paid:true,refunded:true],
+  [amount:80,paid:true,refunded:true],
+  [amount:90,paid:false]
+]
+)
+
+# 3 failed
+nasty_user = User.create(first_name: 'Nasty',last_name:'User')
+nasty_user.charges.create([
+  [amount:90,paid:false],
+  [amount:90,paid:false],
+  [amount:90,paid:false]
+]
+)
+
