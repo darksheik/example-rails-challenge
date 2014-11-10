ac9284f1b36a47f5f34ac9bd307e2aa2e19385db
Don Pflaster <dpflaster@gmail.com>
2014-11-10 17:15:56 -0500

    Charge scopes created

--- source/app/models/charge.rb
+++ source/app/models/charge.rb
@@ -1,3 +1,7 @@
 class Charge < ActiveRecord::Base
   belongs_to :customer, :polymorphic => true
+
+  scope :successful, lambda { where(:paid => true, :refunded => false) }
+  scope :disputed,   lambda { where(:paid => true, :refunded => true) }
+  scope :failed,     lambda { where(:paid => false) }
 end
--- source/spec/models/charge_spec.rb
+++ source/spec/models/charge_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Charge, :type => :model do
   context "associations" do
     it { should belong_to(:customer) }
   end
+
+  context "scopes" do
+    describe "Successful charge" do
+      before {
+        Charge.create!(:paid => true, :refunded => false) #successful
+        Charge.create!(:paid => true, :refunded => true)  #disputed
+        Charge.create!(:paid => false)                    #failed
+      }
+
+      it "should return 1 successful charge" do
+        expect(Charge.successful.count).to eq(1)
+      end
+
+      it "should return 1 disputed charge" do
+        expect(Charge.disputed.count).to eq(1)
+      end
+
+      it "should return 1 failed charge" do
+        expect(Charge.failed.count).to eq(1)
+      end
+    end
+  end
 end
